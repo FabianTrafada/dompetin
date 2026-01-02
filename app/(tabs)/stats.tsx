741f4d8f06fd8ac9834c3d223fde1a55fe38b6e1
FabianTrafada <fabiantrafadaperkasa@gmail.com>
2026-01-02 21:45:32 +0700

    Change border style from dashed to solid in stats screen

--- app/(tabs)/stats.tsx
+++ app/(tabs)/stats.tsx
@@ -443,7 +443,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     borderTopWidth: 1,
-    borderStyle: 'dashed',
+    borderStyle: 'solid',
     zIndex: 0,
   },
   chartScrollContent: {
